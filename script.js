8c38cf1d9e087ea3cd1d37753e4f6076b70e2fd7
Marek Kotulski <marek.kotulski@febrit.co>
2022-06-19 03:37:55 +0200

    test api

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("script")
+const API_URL = "https://zoo-animal-api.herokuapp.com/animals/rand"
+
+fetch(API_URL)
+    .then(response => response.json())
+    .then(data => console.log(data))
